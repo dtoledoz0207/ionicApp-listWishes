c2692d9ac408141e22245ac6bd2d260ac1b3b6b3
David Toledo <dtoledoz0207@gmail.com>
2019-01-18 16:59:24 -0600

    AddListFunctionallity-agregarComponent-Added

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {NavParams} from 'ionic-angular';
 import {DeseosService} from '../../services/deseos.service';
-import {Lista} from '../../models';
+import {Lista, ListaItem} from '../../models';
 
 @Component({
     selector: 'page-agregar',
@@ -10,10 +10,28 @@ import {Lista} from '../../models';
 export class AgregarPage{
 
     lista:Lista;
+    nombreItem:string = '';
 
     constructor(private deseosService: DeseosService, public navParams:NavParams){
        const titulo:string = this.navParams.get('titulo');
        this.lista = new Lista(titulo);
+    }
+
+
+    agregarItem(){
+       if(this.nombreItem.length === 0){
+           return;
+       }else{
+            const nuevoItem = new ListaItem(this.nombreItem);
+            this.lista.items.push(nuevoItem);
+
+            this.nombreItem = '';
+       }
+    }
+
+
+    actualizarTarea(item:ListaItem){
+        item.completado = !item.completado;
     }
 
 }
