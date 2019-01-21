649ad867942436b194eb8c9dd606854b80fa346c
David Toledo <dtoledoz0207@gmail.com>
2019-01-21 13:47:11 -0600

    editListTitle-Added

--- src/components/listas.component.ts
+++ src/components/listas.component.ts
@@ -1,5 +1,5 @@
 import {Component, Input} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController, ItemSliding} from 'ionic-angular';
 import {DeseosService} from '../services/deseos.service';
 
 import {AgregarPage} from '../pages/agregar/agregar.component';
@@ -14,7 +14,7 @@ export class ListasComponent{
 
     @Input() terminada:boolean = false;
 
-    constructor(private deseosService:DeseosService, public navCtrl:NavController){
+    constructor(private deseosService:DeseosService, public navCtrl:NavController, public alertCtrl:AlertController){
 
     }
 
@@ -27,6 +27,40 @@ export class ListasComponent{
 
     borrarLista(lista: Lista){
         this.deseosService.borrarLista(lista);
+    }
+
+
+    editarLista(lista:Lista, slidingItem:ItemSliding){
+
+        slidingItem.close();
+
+        const alerta = this.alertCtrl.create({
+            title: 'Editar nombre',
+            message: 'Editar el nombre de la lista:',
+            inputs: [{
+                name: 'titulo',
+                placeholder: 'Nombre la lista',
+                value: lista.titulo
+            }],
+            buttons:[
+                {
+                    text: 'Cancelar',
+                },
+                {
+                    text: 'Guardar',
+                    handler: data => {
+                        if(data.titulo.lenght === 0){
+                            return;
+                        }
+
+                        lista.titulo = data.titulo;
+                        this.deseosService.guardarStorage();
+                    }
+                }
+            ]
+        });
+
+        alerta.present();
     }
 
 }
